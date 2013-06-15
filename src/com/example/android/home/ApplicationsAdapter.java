99f8b10b5410a29e965c9f131c4ddff2ee8da3a5
rsookram <r@sookram>
2013-06-15 14:33:31 -0400

    Changed items in grid view to image views

--- src/com/example/android/home/ApplicationsAdapter.java
+++ src/com/example/android/home/ApplicationsAdapter.java
@@ -16,7 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 /**
  * GridView adapter to show the list of all installed applications.
@@ -43,7 +43,7 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 			convertView = inflater.inflate(R.layout.application, parent, false);
 		}
 
-		Drawable icon = info.icon;
+		Drawable ic = info.icon;
 
 		if (!info.filtered) {
 			// final Resources resources = getContext().getResources();
@@ -52,18 +52,18 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 			int height = 64;// (int)
 							// resources.getDimension(android.R.dimen.app_icon_size);
 
-			final int iconWidth = icon.getIntrinsicWidth();
-			final int iconHeight = icon.getIntrinsicHeight();
+			final int iconWidth = ic.getIntrinsicWidth();
+			final int iconHeight = ic.getIntrinsicHeight();
 
-			if (icon instanceof PaintDrawable) {
-				PaintDrawable painter = (PaintDrawable) icon;
+			if (ic instanceof PaintDrawable) {
+				PaintDrawable painter = (PaintDrawable) ic;
 				painter.setIntrinsicWidth(width);
 				painter.setIntrinsicHeight(height);
 			}
 
 			if (width > 0 && height > 0
 					&& (width < iconWidth || height < iconHeight)) {
-				final float ratio = (float) iconWidth / iconHeight;
+				float ratio = (float) iconWidth / iconHeight;
 
 				if (iconWidth > iconHeight) {
 					height = (int) (width / ratio);
@@ -71,10 +71,10 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 					width = (int) (height * ratio);
 				}
 
-				final Bitmap.Config c = icon.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+				Bitmap.Config c = ic.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
 						: Bitmap.Config.RGB_565;
-				final Bitmap thumb = Bitmap.createBitmap(width, height, c);
-				final Canvas canvas = new Canvas(thumb);
+				Bitmap thumb = Bitmap.createBitmap(width, height, c);
+				Canvas canvas = new Canvas(thumb);
 				canvas.setDrawFilter(new PaintFlagsDrawFilter(
 						Paint.DITHER_FLAG, 0));
 				// Copy the old bounds to restore them later
@@ -82,19 +82,17 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 				// the call to setBounds() that follows would
 				// change the same instance and we would lose the
 				// old bounds
-				mOldBounds.set(icon.getBounds());
-				icon.setBounds(0, 0, width, height);
-				icon.draw(canvas);
-				icon.setBounds(mOldBounds);
-				icon = info.icon = new BitmapDrawable(activity.getResources(),
+				mOldBounds.set(ic.getBounds());
+				ic.setBounds(0, 0, width, height);
+				ic.draw(canvas);
+				ic.setBounds(mOldBounds);
+				ic = info.icon = new BitmapDrawable(activity.getResources(),
 						thumb);
 				info.filtered = true;
 			}
 		}
 
-		TextView textView = (TextView) convertView.findViewById(R.id.label);
-		textView.setCompoundDrawablesWithIntrinsicBounds(null, icon, null, null);
-		textView.setText(info.title);
+		((ImageView) convertView.findViewById(R.id.img)).setImageDrawable(ic);
 
 		return convertView;
 	}
