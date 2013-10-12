14dc9ddbfdb7309fc7c74907553e03e4ef9243a2
rsookram <r@sookram>
2013-10-11 20:51:44 -0400

    Refactor ApplicationsAdapter
    - replace use of Activity with Context
    - get LayoutInflater in constructor instead of in getView

--- src/com/example/android/home/ApplicationsAdapter.java
+++ src/com/example/android/home/ApplicationsAdapter.java
@@ -1,6 +1,6 @@
 package com.example.android.home;
 
-import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,24 +13,20 @@ import java.util.List;
 /** GridView adapter to show the list of all installed applications. */
 public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 
-    private Activity activity;
-    private List<ApplicationInfo> mApplications;
+    private LayoutInflater mInflater;
 
-    public ApplicationsAdapter(Activity activity,
-                               List<ApplicationInfo> apps) {
-        super(activity, 0, apps);
-        this.activity = activity;
-        this.mApplications = apps;
+    public ApplicationsAdapter(Context context, List<ApplicationInfo> apps) {
+        super(context, 0, apps);
+        mInflater = LayoutInflater.from(context);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
-            LayoutInflater inflater = activity.getLayoutInflater();
-            convertView = inflater.inflate(R.layout.application, parent, false);
+            convertView = mInflater.inflate(R.layout.application, parent, false);
         }
 
-        Drawable ic = mApplications.get(position).icon;
+        Drawable ic = getItem(position).icon;
         ((ImageView) convertView.findViewById(R.id.img)).setImageDrawable(ic);
 
         return convertView;
