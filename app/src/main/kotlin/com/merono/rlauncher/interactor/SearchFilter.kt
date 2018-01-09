7cca22dc1c8e6928136ad4839d0ec376b3255bd9
rsookram <rashadsookram@gmail.com>
2018-01-08 20:30:19 -0500

    Simplify code in SearchFilter and add documentation

--- app/src/main/kotlin/com/merono/rlauncher/interactor/SearchFilter.kt
+++ app/src/main/kotlin/com/merono/rlauncher/interactor/SearchFilter.kt
@@ -2,10 +2,14 @@ package com.merono.rlauncher.interactor
 
 import com.merono.rlauncher.entity.App
 
-fun searchFilter(apps: List<App>, query: CharSequence): List<App> =
-        apps.filter {
-            val normalizedName = "${it.displayName} ${it.packageName}".toLowerCase()
-            val normalizedQuery = query.toString().toLowerCase()
+/**
+ * Returns a filtered version of the given list of apps which contain the given
+ * query string.
+ */
+fun searchFilter(apps: List<App>, query: CharSequence): List<App> {
+    val normalizedQuery = query.toString().toLowerCase()
 
-            normalizedName.contains(normalizedQuery)
-        }
+    return apps.filter {
+        normalizedQuery in "${it.displayName} ${it.packageName}".toLowerCase()
+    }
+}
