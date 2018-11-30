0b4cdf245975344dc81d785614f8b55d265d6197
rsookram <rashadsookram@gmail.com>
2018-11-29 19:53:41 -0500

    Use Data Binding instead of Android Extensions

--- app/src/main/kotlin/io/github/rsookram/rlauncher/view/LauncherView.kt
+++ app/src/main/kotlin/io/github/rsookram/rlauncher/view/LauncherView.kt
@@ -1,23 +1,25 @@
 package io.github.rsookram.rlauncher.view
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.LinearLayout
 import androidx.core.view.updatePadding
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jakewharton.rxbinding2.widget.textChanges
-import io.github.rsookram.rlauncher.R
+import io.github.rsookram.rlauncher.databinding.ViewLauncherBinding
 import io.github.rsookram.rlauncher.entity.App
 import io.reactivex.Observable
-import kotlinx.android.synthetic.main.view_launcher.view.*
 
 class LauncherView(
     context: Context,
     private val appAdapter: AppAdapter
 ) : LinearLayout(context) {
 
+    private val binding: ViewLauncherBinding
+
     val searches: Observable<CharSequence>
-        get() = search_box.textChanges()
+        get() = binding.searchBox.textChanges()
 
     init {
         orientation = VERTICAL
@@ -28,9 +30,9 @@ class LauncherView(
             insets
         }
 
-        inflate(context, R.layout.view_launcher, this)
+        binding = ViewLauncherBinding.inflate(LayoutInflater.from(context), this, true)
 
-        launcher.apply {
+        binding.launcher.apply {
             layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, true)
             adapter = appAdapter
             itemAnimator = null
@@ -48,12 +50,12 @@ class LauncherView(
     }
 
     fun setQuery(query: CharSequence) {
-        if (query.toString() != search_box.text.toString()) {
-            search_box.setText(query)
+        if (query.toString() != binding.searchBox.text.toString()) {
+            binding.searchBox.setText(query)
         }
     }
 
     fun scrollToStart() {
-        launcher.scrollToPosition(0)
+        binding.launcher.scrollToPosition(0)
     }
 }
