6477b807d18be947e95a440fd0b6e183432f52f8
rsookram <rashadsookram@gmail.com>
2019-03-02 11:49:11 -0500

    Move dependencies.gradle to buildSrc
    and convert it to Kotlin

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,26 @@
+object Versions {
+
+    private const val kotlin = "1.3.21"
+    private const val dagger = "2.21"
+
+    val minSdk = 23
+    val targetSdk = 28
+
+    val androidGradlePlugin = "com.android.tools.build:gradle:3.3.1"
+    val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin"
+
+    val appCompat = "androidx.appcompat:appcompat:1.0.2"
+    val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
+    val ktxCore = "androidx.core:core-ktx:1.0.0"
+    val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+    val lifecycleCommon = "androidx.lifecycle:lifecycle-common-java8:2.0.0"
+    val ktxViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+
+    val daggerRuntime = "com.google.dagger:dagger:$dagger"
+    val daggerCompiler = "com.google.dagger:dagger-compiler:$dagger"
+
+    val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlin"
+
+    val coreTesting = "android.arch.core:core-testing:1.1.1"
+    val truth = "com.google.truth:truth:0.42"
+}
