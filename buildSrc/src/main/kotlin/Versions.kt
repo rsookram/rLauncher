4afb1b3af0e10969db51b0c2caa0f4e03a6798a7
rsookram <rashadsookram@gmail.com>
2019-05-02 20:05:28 -0400

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,17 +1,17 @@
 object Versions {
 
-    private const val kotlin = "1.3.21"
-    private const val dagger = "2.21"
+    private const val kotlin = "1.3.31"
+    private const val dagger = "2.22.1"
 
     val minSdk = 23
     val targetSdk = 28
 
-    val androidGradlePlugin = "com.android.tools.build:gradle:3.3.1"
+    val androidGradlePlugin = "com.android.tools.build:gradle:3.4.0"
     val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin"
 
     val appCompat = "androidx.appcompat:appcompat:1.0.2"
     val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
-    val ktxCore = "androidx.core:core-ktx:1.0.0"
+    val ktxCore = "androidx.core:core-ktx:1.0.1"
     val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
     val lifecycleCommon = "androidx.lifecycle:lifecycle-common-java8:2.0.0"
     val ktxViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
