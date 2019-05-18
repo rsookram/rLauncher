6bc9c0186ecbd99a0d47c860c8335575c2cd6854
rsookram <rashadsookram@gmail.com>
2019-05-17 20:52:31 -0400

    Use const to fix IDE warnings

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,24 +3,24 @@ object Versions {
     private const val kotlin = "1.3.31"
     private const val dagger = "2.22.1"
 
-    val minSdk = 23
-    val targetSdk = 28
+    const val minSdk = 23
+    const val targetSdk = 28
 
-    val androidGradlePlugin = "com.android.tools.build:gradle:3.4.0"
-    val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:3.4.0"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin"
 
-    val appCompat = "androidx.appcompat:appcompat:1.0.2"
-    val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
-    val ktxCore = "androidx.core:core-ktx:1.0.1"
-    val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
-    val lifecycleCommon = "androidx.lifecycle:lifecycle-common-java8:2.0.0"
-    val ktxViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.0.2"
+    const val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
+    const val ktxCore = "androidx.core:core-ktx:1.0.1"
+    const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+    const val lifecycleCommon = "androidx.lifecycle:lifecycle-common-java8:2.0.0"
+    const val ktxViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
 
-    val daggerRuntime = "com.google.dagger:dagger:$dagger"
-    val daggerCompiler = "com.google.dagger:dagger-compiler:$dagger"
+    const val daggerRuntime = "com.google.dagger:dagger:$dagger"
+    const val daggerCompiler = "com.google.dagger:dagger-compiler:$dagger"
 
-    val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlin"
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlin"
 
-    val coreTesting = "android.arch.core:core-testing:1.1.1"
-    val truth = "com.google.truth:truth:0.42"
+    const val coreTesting = "android.arch.core:core-testing:1.1.1"
+    const val truth = "com.google.truth:truth:0.42"
 }
