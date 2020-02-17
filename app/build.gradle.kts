7d60298a2df47162b78fe995e977a2213df8bfc3
rsookram <rashadsookram@gmail.com>
2020-02-17 10:30:57 -0500

    Set JVM target to 1.8 for Kotlin compilation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,9 +19,8 @@ android {
         resConfigs("en")
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 
     dataBinding {
