267accf2b3f31c2e805127a296a6db1f76ef5a2b
rsookram <rashadsookram@gmail.com>
2020-03-28 11:43:07 -0400

    Explicitly use default locale
    to fix lint warnings

--- app/src/main/kotlin/io/github/rsookram/rlauncher/interactor/SearchFilter.kt
+++ app/src/main/kotlin/io/github/rsookram/rlauncher/interactor/SearchFilter.kt
@@ -1,15 +1,17 @@
 package io.github.rsookram.rlauncher.interactor
 
 import io.github.rsookram.rlauncher.entity.App
+import java.util.*
 
 /**
  * Returns a filtered version of the given list of apps which contain the given
  * query string.
  */
 fun searchFilter(apps: List<App>, query: CharSequence): List<App> {
-    val normalizedQuery = query.toString().toLowerCase()
+    val defaultLocale = Locale.getDefault()
+    val normalizedQuery = query.toString().toLowerCase(defaultLocale)
 
     return apps.filter {
-        normalizedQuery in "${it.displayName} ${it.packageName}".toLowerCase()
+        normalizedQuery in "${it.displayName} ${it.packageName}".toLowerCase(defaultLocale)
     }
 }
